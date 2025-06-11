1e4bcb4c361b6b0f2033ba972360c2e27de88790
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-11 22:00:21 +0800

    [milky] add inheriting types to MilkyGroupRequest

--- saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/model/request/MilkyGroupRequest.kt
+++ saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/model/request/MilkyGroupRequest.kt
@@ -1,6 +1,25 @@
 package org.ntqqrev.milky.model.request
 
-internal class MilkyGroupRequest(
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "request_type",
+    visible = true
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(
+        value = MilkyGroupJoinRequest::class,
+        name = "join"
+    ),
+    JsonSubTypes.Type(
+        value = MilkyGroupInviteRequest::class,
+        name = "invite"
+    )
+)
+internal sealed class MilkyGroupRequest(
     val requestId: String,
     val time: Long,
     val isFiltered: Boolean,
@@ -8,7 +27,26 @@ internal class MilkyGroupRequest(
     val state: String,
     val groupId: Long,
     val operatorId: Long? = null,
-    val requestType: String,
-    val comment: String? = null,
-    val inviteeId: Long? = null,
 )
+
+internal class MilkyGroupJoinRequest(
+    requestId: String,
+    time: Long,
+    isFiltered: Boolean,
+    initiatorId: Long,
+    state: String,
+    groupId: Long,
+    operatorId: Long? = null,
+    val comment: String? = null,
+) : MilkyGroupRequest(requestId, time, isFiltered, initiatorId, state, groupId, operatorId)
+
+internal class MilkyGroupInviteRequest(
+    requestId: String,
+    time: Long,
+    isFiltered: Boolean,
+    initiatorId: Long,
+    state: String,
+    groupId: Long,
+    operatorId: Long? = null,
+    val inviteeId: Long,
+) : MilkyGroupRequest(requestId, time, isFiltered, initiatorId, state, groupId, operatorId)
