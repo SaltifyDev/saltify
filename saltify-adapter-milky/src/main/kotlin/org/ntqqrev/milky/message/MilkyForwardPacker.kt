5b453141180fb99f043716a07351572035dc95d9
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-15 23:21:51 +0800

    [milky] implement incoming -> outgoing conversion

--- saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/message/MilkyForwardPacker.kt
+++ saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/message/MilkyForwardPacker.kt
@@ -1,25 +1,94 @@
 package org.ntqqrev.milky.message
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import org.ntqqrev.milky.MilkyContext
 import org.ntqqrev.milky.model.message.MilkyOutgoingForwardedMessageData
-import org.ntqqrev.saltify.message.incoming.IncomingMessage
+import org.ntqqrev.milky.model.message.*
+import org.ntqqrev.saltify.getMessages
+import org.ntqqrev.saltify.getTempUrl
+import org.ntqqrev.saltify.message.incoming.*
 import org.ntqqrev.saltify.message.outgoing.ForwardFeature
 import org.ntqqrev.saltify.message.outgoing.ForwardMessageBuilder
 
 class MilkyForwardPacker(val ctx: MilkyContext) : ForwardFeature.Packer {
+    private val deferredData = mutableListOf<Deferred<MilkyOutgoingForwardedMessageData>>()
+
+    private fun MutableList<Deferred<MilkyOutgoingForwardedMessageData>>.addAsync(
+        block: suspend () -> MilkyOutgoingForwardedMessageData
+    ) {
+        this.add(ctx.env.scope.async { block() })
+    }
+
     override fun fake(
         uin: Long,
         name: String,
         builder: ForwardMessageBuilder.() -> Unit
     ) {
-        TODO("Not yet implemented")
+        val forwardBuilder = MilkyUniversalMessageBuilder(ctx)
+        forwardBuilder.builder()
+        deferredData.addAsync {
+            MilkyOutgoingForwardedMessageData(
+                userId = uin,
+                name = name,
+                segments = forwardBuilder.build()
+            )
+        }
     }
 
     override fun existing(incomingMessage: IncomingMessage) {
-        TODO("Not yet implemented")
+        when (incomingMessage) {
+            is PrivateIncomingMessage -> {
+                deferredData.addAsync {
+                    val userId: Long
+                    val name: String
+                    if (incomingMessage.isSelf) {
+                        val (aUserId, aName) = incomingMessage.ctx.getLoginInfo()
+                        userId = aUserId
+                        name = aName
+                    } else {
+                        userId = incomingMessage.peer.uin
+                        name = incomingMessage.peer.nickname
+                    }
+                    MilkyOutgoingForwardedMessageData(
+                        userId, name,
+                        segments = incomingMessage.segments.map {
+                            MilkyOutgoingSegmentModel(it.toMilkyOutgoingData())
+                        }
+                    )
+                }
+            }
+        }
     }
 
-    internal fun build(): List<MilkyOutgoingForwardedMessageData> {
-        TODO("Not yet implemented")
-    }
+    private suspend fun Segment.toMilkyOutgoingData(): MilkyOutgoingData =
+        when (this) {
+            is TextSegment -> MilkyOutgoingTextData(text)
+            is MentionSegment -> if (uin != null)
+                MilkyOutgoingMentionData(uin!!)
+            else
+                MilkyOutgoingMentionAllData()
+            is FaceSegment -> MilkyOutgoingFaceData(id)
+            is ReplySegment -> MilkyOutgoingReplyData(repliedSequence)
+            is ImageSegment -> MilkyOutgoingImageData(
+                uri = getTempUrl(),
+                subType = subType.name,
+                summary = summary
+            )
+            is RecordSegment -> MilkyOutgoingRecordData(getTempUrl())
+            is VideoSegment -> MilkyOutgoingVideoData(getTempUrl())
+            is ForwardSegment -> MilkyOutgoingForwardData(getMessages().map {
+                MilkyOutgoingForwardedMessageData(
+                    userId = 0,
+                    name = it.senderName,
+                    segments = it.segments.map { segment ->
+                        MilkyOutgoingSegmentModel(segment.toMilkyOutgoingData())
+                    }
+                )
+            })
+            else -> throw IllegalArgumentException("Unsupported segment type: ${this::class.simpleName}")
+        }
+
+    internal suspend fun build(): List<MilkyOutgoingForwardedMessageData> =
+        deferredData.map { it.await() }
 }
