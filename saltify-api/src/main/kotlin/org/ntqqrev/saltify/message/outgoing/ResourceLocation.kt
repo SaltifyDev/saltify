c86c541ba32a25de3fdf96f69c7e5e27958997f0
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-15 15:58:59 +0800

    [api] add util functions to shorten the construction of ResourceLocation

--- saltify-api/src/main/kotlin/org/ntqqrev/saltify/message/outgoing/ResourceLocation.kt
+++ saltify-api/src/main/kotlin/org/ntqqrev/saltify/message/outgoing/ResourceLocation.kt
@@ -9,3 +9,19 @@ class LocalResource(val path: String) : ResourceLocation()
 class RemoteResource(val url: String) : ResourceLocation()
 
 class RawResource(val stream: InputStream) : ResourceLocation()
+
+fun local(path: String): LocalResource {
+    return LocalResource(path)
+}
+
+fun remote(url: String): RemoteResource {
+    return RemoteResource(url)
+}
+
+fun raw(stream: InputStream): RawResource {
+    return RawResource(stream)
+}
+
+fun raw(bytes: ByteArray): RawResource {
+    return RawResource(bytes.inputStream())
+}
