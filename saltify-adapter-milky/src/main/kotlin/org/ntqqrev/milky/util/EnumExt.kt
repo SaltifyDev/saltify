8c16ead5769a20780de7d9292b8f6bad10b844e9
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-15 22:12:17 +0800

    [milky] move enum extension methods to one file

--- saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/util/EnumExt.kt
+++ saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/util/EnumExt.kt
@@ -1,6 +1,20 @@
 package org.ntqqrev.milky.util
 
+import org.ntqqrev.saltify.model.Gender
 import org.ntqqrev.saltify.model.GroupMember
+
+internal fun String.toSaltifyGender() = when (this) {
+    "male" -> Gender.MALE
+    "female" -> Gender.FEMALE
+    "unknown" -> Gender.UNKNOWN
+    else -> throw IllegalArgumentException("Invalid sex")
+}
+
+internal fun Gender.toMilkySex(): String = when (this) {
+    Gender.MALE -> "male"
+    Gender.FEMALE -> "female"
+    Gender.UNKNOWN -> "unknown"
+}
 
 internal fun String.toSaltifyRole() = when (this) {
     "owner" -> GroupMember.Role.OWNER
