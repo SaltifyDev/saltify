09e9c96f1691d4ae0fa963c7be151e8f584f0ae3
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-17 20:31:27 +0800

    [milky] change field nullity according to protocol change

--- saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/protocol/entity/MilkyGroupFileData.kt
+++ saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/protocol/entity/MilkyGroupFileData.kt
@@ -4,10 +4,10 @@ class MilkyGroupFileData(
     val groupId: Long,
     val fileId: String,
     val fileName: String,
-    val parentFolderId: String? = null,
+    val parentFolderId: String,
     val fileSize: Long,
     val uploadedTime: Long,
-    val expireTime: Long,
+    val expireTime: Long? = null,
     val uploaderId: Long,
     val downloadedTimes: Int,
 )
--- saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/protocol/entity/MilkyGroupFolderData.kt
+++ saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/protocol/entity/MilkyGroupFolderData.kt
@@ -3,7 +3,7 @@ package org.ntqqrev.milky.protocol.entity
 class MilkyGroupFolderData(
     val groupId: Long,
     val folderId: String,
-    val parentFolderId: String? = null,
+    val parentFolderId: String,
     val folderName: String,
     val createdTime: Long,
     val lastModifiedTime: Long,
