e99916ccb97e6520efaa729e44ec2497062395e6
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-18 13:56:54 +0800

    [core] refactor command node

--- saltify/src/main/kotlin/org/ntqqrev/saltify/command/CommandNode.kt
+++ saltify/src/main/kotlin/org/ntqqrev/saltify/command/CommandNode.kt
@@ -1,50 +1,37 @@
 package org.ntqqrev.saltify.command
 
-sealed class CommandNode<T>(
-    val parent: CommandNode<*>?
-) {
-    internal val children: MutableList<CommandNode<*>> = mutableListOf()
+import org.ntqqrev.saltify.dsl.ParamCapturer
 
+sealed class CommandNode<T>(
+    val name: String,
+    val description: String,
+) : ParamCapturer<T> {
     abstract fun tryMatch(token: String): T?
 }
 
-class LiteralNode(
-    parent: CommandNode<*>?,
-    private val literal: String
-) : CommandNode<String>(parent) {
-    override fun tryMatch(token: String): String? {
-        return if (token.equals(literal, ignoreCase = true)) literal else null
-    }
-}
-
-class IntNode(
-    parent: CommandNode<*>?
-) : CommandNode<Int>(parent) {
+class IntNode(name: String, description: String) : CommandNode<Int>(name, description) {
     override fun tryMatch(token: String): Int? {
         return token.toIntOrNull()
     }
 }
 
-class LongNode(
-    parent: CommandNode<*>?
-) : CommandNode<Long>(parent) {
+class LongNode(name: String, description: String) : CommandNode<Long>(name, description) {
     override fun tryMatch(token: String): Long? {
         return token.toLongOrNull()
     }
 }
 
-class DoubleNode(
-    parent: CommandNode<*>?
-) : CommandNode<Double>(parent) {
+class DoubleNode(name: String, description: String) : CommandNode<Double>(name, description) {
     override fun tryMatch(token: String): Double? {
         return token.toDoubleOrNull()
     }
 }
 
 class StringNode(
-    parent: CommandNode<*>?,
+    name: String,
+    description: String,
     val isGreedy: Boolean = false
-) : CommandNode<String>(parent) {
+) : CommandNode<String>(name, description) {
     override fun tryMatch(token: String): String? {
         return token
     }
