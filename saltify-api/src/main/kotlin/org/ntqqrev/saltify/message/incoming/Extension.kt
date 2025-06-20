ce12c396e707ba3eb19b2f231e8ac0ff8799668e
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-20 20:18:15 +0800

    [api] add isMentioningSelf extension

--- saltify-api/src/main/kotlin/org/ntqqrev/saltify/message/incoming/Extension.kt
+++ saltify-api/src/main/kotlin/org/ntqqrev/saltify/message/incoming/Extension.kt
@@ -1,3 +1,13 @@
 package org.ntqqrev.saltify.message.incoming
 
 val PrivateIncomingMessage.isSelf get() = sender == peer
+
+suspend fun GroupIncomingMessage.isMentioningSelf(): Boolean {
+    val selfUin = ctx.getLoginInfo().first
+    for (segment in segments) {
+        if (segment is MentionSegment && segment.uin == selfUin) {
+            return true
+        }
+    }
+    return false
+}
