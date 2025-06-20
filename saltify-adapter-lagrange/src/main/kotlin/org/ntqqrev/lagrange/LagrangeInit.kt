eaa17dfc089e1714ca00c9078a70a19fc2a926aa
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-20 22:22:58 +0800

    [all] make config classes have all args constructor

--- saltify-adapter-lagrange/src/main/kotlin/org/ntqqrev/lagrange/LagrangeInit.kt
+++ saltify-adapter-lagrange/src/main/kotlin/org/ntqqrev/lagrange/LagrangeInit.kt
@@ -2,21 +2,21 @@ package org.ntqqrev.lagrange
 
 import org.ntqqrev.saltify.config.Configurable
 
-class LagrangeInit {
+class LagrangeInit(
     @Configurable(
         "签名 API URL"
     )
-    val signApiUrl: String = "https://sign.lagrangecore.org/api/sign/30366"
+    val signApiUrl: String = "https://sign.lagrangecore.org/api/sign/30366",
 
     @Configurable(
         "签名 API 代理 URL",
         "如果访问签名 API URL 失败，可以尝试使用代理，仅支持 HTTP 代理"
     )
-    val signApiProxyUrl: String = ""
+    val signApiProxyUrl: String = "",
 
     @Configurable(
         "QQ 号",
         "必须与扫码时所用 QQ 号相同，否则无法正常自动登录"
     )
-    val uin: Long = 0L
-}
+    val uin: Long = 0L,
+)
--- saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/MilkyInit.kt
+++ saltify-adapter-milky/src/main/kotlin/org/ntqqrev/milky/MilkyInit.kt
@@ -3,22 +3,22 @@ package org.ntqqrev.milky
 import org.ntqqrev.saltify.config.Configurable
 import org.ntqqrev.saltify.config.LongRange
 
-class MilkyInit {
+class MilkyInit(
     @Configurable(
         "Milky 服务 URL",
         "仅支持 HTTP API + WebSocket 事件推送"
     )
-    val milkyUrl: String = "http://localhost:8080/"
+    val milkyUrl: String = "http://localhost:8080/",
 
     @Configurable(
         "Milky 的 Access Token"
     )
-    val milkyAccessToken: String = ""
+    val milkyAccessToken: String = "",
 
     @Configurable(
         "WebSocket 重连间隔",
         "单位毫秒 (ms)"
     )
     @LongRange(min = 5000L)
-    val wsReconnectInterval: Long = 5000L
-}
+    val wsReconnectInterval: Long = 5000L,
+)
