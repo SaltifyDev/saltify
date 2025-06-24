173f903322b32432b4dff34c8d6f310186434a82
Wesley F. Young <wesley.f.young@outlook.com>
2025-06-24 22:36:30 +0800

    [web] implement base layout

--- saltify-web/src/App.tsx
+++ saltify-web/src/App.tsx
@@ -1,9 +1,28 @@
+import { Avatar, Layout, Menu, theme } from 'antd';
+import logo from './logo.png';
+
 function App() {
-  return (
-    <>
-      Saltify Web App
-    </>
-  )
+  const {
+    token: { colorTextLightSolid, colorBgContainer },
+  } = theme.useToken();
+
+  return <Layout style={{ minHeight: '100vh' }}>
+    <Layout.Header style={{ display: 'flex', alignItems: 'center', paddingLeft: 16 }}>
+      <Avatar src={logo}/>
+      <div style={{ color: colorTextLightSolid }}><h2>Saltify</h2></div>
+    </Layout.Header>
+    <Layout>
+      <Layout.Sider width={200} style={{ background: colorBgContainer }}>
+        <Menu
+          mode="inline"
+          style={{ padding: 8, height: '100%' }}
+          items={[
+
+          ]}
+        />
+      </Layout.Sider>
+    </Layout>
+  </Layout>;
 }
 
-export default App
+export default App;
