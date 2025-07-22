7f4bad959512fa1c5c4aee01a2a474201e67f727
Wesley F. Young <wesley.f.young@outlook.com>
2025-07-22 17:50:31 +0800

    feat(web): add react-router and home page

---
+++ saltify-web/src/Home.tsx
@@ -0,0 +1,11 @@
+import { Typography } from 'antd';
+
+function Home() {
+  return (
+    <div>
+      <Typography><h1>Welcome to Saltify!</h1></Typography>
+    </div>
+  )
+}
+
+export default Home;
--- saltify-web/src/main.tsx
+++ saltify-web/src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.tsx';
+import Home from './Home.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
