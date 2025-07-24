1b658f6eaeb1576dc54829b5c75929cccc8cfbeb
Wesley F. Young <wesley.f.young@outlook.com>
2025-07-24 23:52:19 +0800

    refactor(core): simplify message tokenizing logic

--- saltify/src/main/kotlin/org/ntqqrev/saltify/command/MessageTokenizer.kt
+++ saltify/src/main/kotlin/org/ntqqrev/saltify/command/MessageTokenizer.kt
@@ -3,63 +3,48 @@ package org.ntqqrev.saltify.command
 import org.ntqqrev.saltify.message.incoming.IncomingMessage
 import org.ntqqrev.saltify.message.incoming.TextSegment
 
+private const val segmentTokenPrefix = 0x1F.toChar()
+
 class MessageTokenizer(
     private val message: IncomingMessage,
     startIndex: Int = 0
 ) : ITokenizer<Token> {
-    private var index = startIndex
-    private var currentTextTokenizer: TextTokenizer? = null
-    private var textHasBeenRead = false
+    private val textTokenizer = TextTokenizer(
+        message.segments
+            .drop(startIndex)
+            .mapIndexed { index, segment ->
+                when (segment) {
+                    is TextSegment -> segment.text
+                    else -> "$segmentTokenPrefix${index + startIndex}"
+                }
+            }
+            .joinToString(" ")
+    )
 
-    override fun hasMoreTokens(): Boolean =
-        index < message.segments.size || (currentTextTokenizer?.hasMoreTokens() ?: false)
+    override fun hasMoreTokens() = textTokenizer.hasMoreTokens()
 
     override fun read(): Token {
-        if (currentTextTokenizer != null) {
-            textHasBeenRead = true
-            if (currentTextTokenizer!!.hasMoreTokens()) {
-                return TextToken(currentTextTokenizer!!.read())
-            } else {
-                currentTextTokenizer = null
-                index++
-                return read()
+        val rawToken = textTokenizer.read()
+        return when {
+            rawToken.startsWith(segmentTokenPrefix) -> {
+                val index = rawToken.substring(1).toInt()
+                SegmentToken(message.segments[index])
             }
-        }
-        val segment = message.segments[index]
-        if (segment is TextSegment) {
-            currentTextTokenizer = TextTokenizer(segment.text)
-            textHasBeenRead = false
-            return read()
-        } else {
-            index++
-            return SegmentToken(segment)
+            else -> TextToken(rawToken)
         }
     }
 
     override fun unread() {
-        if (currentTextTokenizer != null) {
-            if (textHasBeenRead) {
-                currentTextTokenizer!!.unread()
-                return
-            } else {
-                currentTextTokenizer = null
-            }
-        }
-        if (index == 0)
-            throw IllegalStateException("Cannot unread beyond the beginning of segments.")
-        index--
+        textTokenizer.unread()
     }
 
     override fun remaining(): Token {
-        if (index < message.segments.size || currentTextTokenizer == null) {
+        val rawToken = textTokenizer.remaining()
+        if (segmentTokenPrefix in rawToken) {
             throw IllegalStateException(
-                "Cannot read remaining token when there are segments other than text segments left."
+                "Cannot read remaining token when it contains segments"
             )
         }
-        if (currentTextTokenizer!!.hasMoreTokens()) {
-            return TextToken(currentTextTokenizer!!.remaining())
-        } else {
-            throw NoSuchElementException("No more tokens available in the message.")
-        }
+        return TextToken(rawToken)
     }
 }
