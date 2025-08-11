f73ed646ebb6d569644c12c203b2d5c724070b22
Wesley F. Young <wesley.f.young@outlook.com>
2025-08-11 12:04:44 +0800

    feat(web): use SideNavItem to construct navigations

--- saltify-web/src/App.tsx
+++ saltify-web/src/App.tsx
@@ -1,34 +1,48 @@
-import { Box, Flex, Text, Avatar, HStack } from '@chakra-ui/react';
+import { Box, Flex, Text, Avatar, HStack, VStack } from '@chakra-ui/react';
 import { Outlet } from 'react-router';
 import logo from './assets/logo.png';
+import SideNavItem from './components/SideNavItem.tsx';
+
+interface NavItem {
+  href: string;
+  title: string;
+}
+
+const navItems: NavItem[] = [{ href: '/', title: '首页' }];
 
 function App() {
   return (
     <Flex direction={'column'} h={'100vh'}>
       {/* App Bar */}
-      <Box as={'header'} background={'teal.500'} color={'white'} paddingX={6} paddingY={4} boxShadow={'md'}>
+      <Box as={'header'} bg={'teal.500'} color={'white'} px={6} py={4} boxShadow={'md'}>
         <HStack>
           <Avatar.Root>
             <Avatar.Fallback name={'Saltify'} />
             <Avatar.Image src={logo} />
           </Avatar.Root>
-          <Text fontSize={'xl'} fontWeight={'bold'}>Saltify</Text>
+          <Text fontSize={'xl'} fontWeight={'bold'}>
+            Saltify
+          </Text>
         </HStack>
       </Box>
       <Flex flex={'1'} minH={0}>
         {/* Sidebar */}
         <Box
           as={'nav'}
-          width={'15%'}
-          minWidth={200}
+          w={'15%'}
           h={'full'}
-          background={'gray.100'}
-          padding={4}
+          minW={200}
+          bg={'gray.50'}
+          p={4}
           boxShadow={'md'}
           position={'sticky'}
           top={0}
         >
-          {/* TODO: Navi bar */}
+          <VStack align={'stretch'}>
+            {navItems.map((item) => (
+              <SideNavItem key={item.href} href={item.href} title={item.title} />
+            ))}
+          </VStack>
         </Box>
         {/* Main Content */}
         <Box as={'main'} flex={'1'} padding={6} overflowY={'auto'} background={'white'}>
