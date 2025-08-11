7873f8f54c83b4ea82b006e911f6ccb3e8887c1b
Wesley F. Young <wesley.f.young@outlook.com>
2025-08-11 12:05:18 +0800

    refactor(web): pages directory structure

--- saltify-web/src/main.tsx
+++ saltify-web/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import App from './App.tsx';
-import Home from './Home.tsx';
+import Home from './pages/Home.tsx';
 import { Provider } from './components/ui/provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
