1190e024e4e20a420256a4575063baaae4e648a5
Wesley F. Young <wesley.f.young@outlook.com>
2025-08-12 18:05:28 +0800

    fix(api): correct annotation target of Range and Configurable

--- saltify-api/src/main/kotlin/org/ntqqrev/saltify/config/Configurable.kt
+++ saltify-api/src/main/kotlin/org/ntqqrev/saltify/config/Configurable.kt
@@ -3,5 +3,5 @@ package org.ntqqrev.saltify.config
 /**
  * Describes an option that can be configured.
  */
-@Target(AnnotationTarget.FIELD)
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
 annotation class Configurable(val name: String, val description: String = "")
--- saltify-api/src/main/kotlin/org/ntqqrev/saltify/config/Range.kt
+++ saltify-api/src/main/kotlin/org/ntqqrev/saltify/config/Range.kt
@@ -1,6 +1,13 @@
 package org.ntqqrev.saltify.config
 
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
 annotation class IntRange(val min: Int = 0, val max: Int = Int.MAX_VALUE)
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
 annotation class LongRange(val min: Long = 0, val max: Long = Long.MAX_VALUE)
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
 annotation class FloatRange(val min: Float = 0f, val max: Float = Float.MAX_VALUE)
+
+@Target(AnnotationTarget.FIELD, AnnotationTarget.PROPERTY)
 annotation class DoubleRange(val min: Double = 0.0, val max: Double = Double.MAX_VALUE)
