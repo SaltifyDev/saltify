43863ef85db0eeb6345c33643ec7d38d66bf8cf2
Wei FeiYang <wesley.f.young@outlook.com>
2025-12-23 23:27:23 +0800

    update: kotlin -> 2.3.0; ktor -> 3.3.3; add ksp module

---
+++ sdk/build.gradle.kts
@@ -0,0 +1,99 @@
+@file:OptIn(ExperimentalWasmDsl::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    kotlin("multiplatform") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
+    id("com.vanniktech.maven.publish") version "0.35.0"
+}
+
+kotlin {
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX64()
+    androidNativeX86()
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    js(IR) {
+        browser()
+        nodejs()
+    }
+    jvm()
+    linuxArm64()
+    linuxX64()
+    macosArm64()
+    macosX64()
+    mingwX64()
+    tvosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+    }
+    watchosArm32()
+    watchosArm64()
+    watchosDeviceArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(ktorLibs.client.core)
+            implementation(ktorLibs.client.contentNegotiation)
+            implementation(ktorLibs.client.serialization)
+            implementation(ktorLibs.client.websockets)
+            implementation(ktorLibs.serialization.kotlinx.json)
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+            implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+            api("org.ntqqrev:milky-kt-types:1.0.0")
+        }
+
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+        jvmTest.dependencies {
+            implementation(ktorLibs.client.cio)
+        }
+    }
+
+    jvmToolchain(21)
+}
+
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates(
+        groupId = rootProject.group.toString(),
+        artifactId = "milky-kt-sdk",
+        version = rootProject.version.toString()
+    )
+
+    pom {
+        name = rootProject.name
+        description = "Milky SDK for Kotlin Multiplatform"
+        url = "https://github.com/SaltifyDev/milky-kt-sdk"
+        inceptionYear = "2025"
+        licenses {
+            license {
+                name = "MIT License"
+                url = "https://opensource.org/licenses/MIT"
+            }
+        }
+        developers {
+            developer {
+                id = "Wesley-Young"
+                name = "Wesley F. Young"
+                email = "wesley.f.young@outlook.com"
+            }
+        }
+        scm {
+            connection = "scm:git:git://github.com/SaltifyDev/milky-kt-sdk.git"
+            developerConnection = "scm:git:ssh://github.com/SaltifyDev/milky-kt-sdk.git"
+            url = "https://github.com/SaltifyDev/milky-kt-sdk"
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,15 @@ dependencyResolutionManagement {
 
     versionCatalogs {
         create("ktorLibs") {
-            from("io.ktor:ktor-version-catalog:3.3.2")
+            from("io.ktor:ktor-version-catalog:3.3.3")
         }
     }
 }
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "milky-kt-sdk"
+
+include(":sdk", ":ksp")
