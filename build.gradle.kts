37f4cf255a2e0bf55bfc1dcaeb906b9f9ac3e8f4
Wei FeiYang <wesley.f.young@outlook.com>
2026-01-10 13:48:40 +0800

    update: bump version to 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "org.ntqqrev"
-version = "1.1.0-Beta2"
+version = "1.1.0"
 
 subprojects {
     repositories {
--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -58,8 +58,8 @@ kotlin {
                 implementation(ktorLibs.client.websockets)
                 implementation(ktorLibs.serialization.kotlinx.json)
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
-                api("org.ntqqrev:milky-kt-types:1.1.0-RC5.1")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0-RC")
+                api("org.ntqqrev:milky-kt-types:1.1.0")
             }
         }
 
