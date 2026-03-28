bc48f71aebfb5c9668a589b36af073acfe67a744
Aliorpse <Aliorpse@qq.com>
2026-03-28 17:47:16 +0800

    feat: add isMention requirement

--- saltify-core/src/commonMain/kotlin/org/ntqqrev/saltify/extension/RequirementExtension.kt
+++ saltify-core/src/commonMain/kotlin/org/ntqqrev/saltify/extension/RequirementExtension.kt
@@ -1,6 +1,7 @@
 package org.ntqqrev.saltify.extension
 
 import org.ntqqrev.milky.IncomingMessage
+import org.ntqqrev.milky.IncomingSegment
 import org.ntqqrev.saltify.entity.SaltifyCommandRequirementContext
 import org.ntqqrev.saltify.model.CommandRequirement
 import org.ntqqrev.saltify.model.PermissionLevel
@@ -34,3 +35,9 @@ public val SaltifyCommandRequirementContext.isGroupOwner: CommandRequirement
 
 public val SaltifyCommandRequirementContext.isGroupAdminOrOwner: CommandRequirement
     get() = isGroupAdmin or isGroupOwner
+
+public val SaltifyCommandRequirementContext.isMention: CommandRequirement
+    get() = CommandRequirement {
+        val segment = context.event.segments.filterIsInstance<IncomingSegment.Mention>()
+        segment.isNotEmpty() && segment.any { it.userId == context.event.senderId }
+    }
